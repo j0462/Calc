c489f27189aefe9a4b32ff9ab5befb45221c10a2
LSH <k01094060462@gmail.com>
2024-04-22 19:18:15 +0900

    정수 입력 받기

--- src/App.java
+++ src/App.java
@@ -1,4 +1,15 @@
+import java.util.Scanner; //스캐너 라이브러리
+
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //스캐너 생성
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int a = sc.nextInt(); //첫 번째 인수
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int b = sc.nextInt(); //두 번째 인수
+
+        sc.close(); //스캐너 종료 컴파일 오류 예방
     }
 }
