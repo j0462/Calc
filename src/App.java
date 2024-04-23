82ffaca5876ac66f3e6096410589f65f62ea4825
LSH <k01094060462@gmail.com>
2024-04-23 16:02:21 +0900

    원넓이 기능 추가

--- src/App.java
+++ src/App.java
@@ -5,45 +5,74 @@ import java.util.List; //리스트 라이브러리
 
 public class App {
     public static void main(String[] args) {
-        while (true) {
-            Scanner sc = new Scanner(System.in); //스캐너 생성
+        Scanner sc = new Scanner(System.in); //스캐너 생성
 
-            System.out.print("첫 번째 숫자(양수)를 입력하세요: ");
-            int a = sc.nextInt(); //첫 번째 인수
+        System.out.print("산수 : m | 원넓이 : c 를 입력해주세요: "); // 산수, 원넓이 선택
+        Calculator calculator = new Calculator(sc); //클래스 생성자
 
-            System.out.print("두 번째 숫자(양수)를 입력하세요: ");
-            int b = sc.nextInt(); //두 번째 인수
-
-            int result = 0;
-
-            if (a >= 0 && b >= 0) { //두 인수의 음수 판별
-                System.out.print("사칙연산 기호를 입력하세요: ");
-                char c = sc.next().charAt(0); //사칙연산 기호
-
-                Calculator calculator = new Calculator();
-                result = calculator.Calc(a,b,c);
-                if(c=='/' && b==0)
-                {
-                    System.out.println("결과 : ERROR");
-                }else {
-                    System.out.println("결과 : "+result);
+        if(calculator.Getmode().equals("circle")){
+            while (true){
+                System.out.print("반지름을 입력해주세요: ");
+                double a = sc.nextDouble(); //반지름 인수
+                double result = 0;
+                try{
+                    if(a>=0) {
+                        result = calculator.calculateCircleArea(a);
+                    }else{
+                        throw new Exception("반지름이 마이너스");
+                    }
+                }catch (Exception e){
+                    System.out.println("반지름이 음수가 될 수 없습니다");
                 }
-            } else {
-                System.out.println("입력하신 숫자 중 음수가 있습니다");
+
+                System.out.println("결과 : "+result);
+
+                calculator.CRemoveArray(sc); //삭제 메소드
+
+                calculator.CInquiryArray(sc); //조회 메소드
+
+                System.out.println("\n더 계산하시겠습니까? (exit 입력 시 종료)"); //종료 트리거
+                String q = sc.next();
+                if(q.equals("exit")) {
+                    break; //탈출
+                }
             }
+        } else if(calculator.Getmode().equals("math")){ //산수기능
+            while (true) {
+                System.out.print("첫 번째 숫자(양수)를 입력하세요: ");
+                int a = sc.nextInt(); //첫 번째 인수
 
-            Calculator calculator = new Calculator(); //클래스 생성자
+                System.out.print("두 번째 숫자(양수)를 입력하세요: ");
+                int b = sc.nextInt(); //두 번째 인수
 
-            calculator.RemoveArray(sc); //삭제 메소드
+                int result = 0;
 
-            calculator.InquiryArray(sc); //조회 메소드
+                if (a >= 0 && b >= 0) { //두 인수의 음수 판별
+                    System.out.print("사칙연산 기호를 입력하세요: ");
+                    char c = sc.next().charAt(0); //사칙연산 기호
 
-            System.out.println("\n더 계산하시겠습니까? (exit 입력 시 종료)"); //종료 트리거
-            String q = sc.next();
-            if(q.equals("exit")) {
-                sc.close(); //스캐너 종료 컴파일 오류 예방
-                break; //탈출
+                    result = calculator.Calc(a,b,c);
+                    if(c=='/' && b==0)
+                    {
+                        System.out.println("결과 : ERROR");
+                    }else {
+                        System.out.println("결과 : "+result);
+                    }
+                } else {
+                    System.out.println("입력하신 숫자 중 음수가 있습니다");
+                }
+
+                calculator.RemoveArray(sc); //삭제 메소드
+
+                calculator.InquiryArray(sc); //조회 메소드
+
+                System.out.println("\n더 계산하시겠습니까? (exit 입력 시 종료)"); //종료 트리거
+                String q = sc.next();
+                if(q.equals("exit")) {
+                    break; //탈출
+                }
             }
         }
+        sc.close(); //스캐너 종료 컴파일 오류 예방
     }
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -3,18 +3,30 @@ import java.util.List;
 import java.util.Scanner; //스캐너 라이브러리
 
 public class Calculator {
-    static List<Integer> resultArray; //public 생략시 private
+    List<Integer> resultArray; //public 생략시 private
+    static final double PI = 3.141592; //접근제한이 필요없기에 static, 파이는 고정값이라 덮어쓸 일이 없기에 final이 붙는다
+    List<Double> circleResult; //원 넓이 저장
+    String mode;
 
-    public Calculator() { //생성자 초기화
-        resultArray = new ArrayList<>();
+    public Calculator(Scanner sc) { //생성자 초기화
+        char c = sc.next().charAt(0);
+        if(c == 'm'){
+            resultArray = new ArrayList<>();
+            mode = "math";
+        }else if(c == 'c'){
+            circleResult = new ArrayList<>();
+            mode = "circle";
+        }
     }
 
-    public List<Integer> GetArray(){ //Getter 메소드
-        return resultArray;
+    public String Getmode() {
+        return mode;
     }
 
-    public void SetArray(List<Integer> array){ //Setter 메소드
-        resultArray = array;
+    public double calculateCircleArea(double r) {
+        double area = r*r*PI;
+        CAddArray(area);
+        return area;
     }
 
     public int Calc(int a, int b, char c){ //계산
@@ -54,6 +66,25 @@ public class Calculator {
                 return 0;
         }
         return result; //반환값
+    }
+    public void CAddArray(double a){ //원넓이 결과를 저장하는 메소드
+        circleResult.add(a);
+    }
+    public void CRemoveArray(Scanner sc){ //원넓이 결과를 삭제하는 메소드 setter
+        System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+        String r = sc.next();
+        if(r.equals("remove")) {
+            circleResult.removeFirst();
+        }
+    }
+    public void CInquiryArray(Scanner sc){ //원넓이 결과를 조회하는 메소드 getter
+        System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+        String i = sc.next();
+        if(i.equals("inquiry")) {
+            for(double unit : circleResult) {  //향상된 for문
+                System.out.print(unit + " ");
+            }
+        }
     }
     public void AddArray(int a){ //결과를 저장하는 메소드
         resultArray.add(a);
