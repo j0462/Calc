0c1fc17c2e6caf7f4a173aa7b877d1eaa237b5fd
LSH <k01094060462@gmail.com>
2024-05-03 10:52:47 +0900

    오류 수정

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,11 +6,23 @@ public class Calculator {
     String mode;
 
     public Calculator(Scanner sc) { //생성자 초기화
-        char c = sc.next().charAt(0);
-        if(c == 'm'){
-            mode = "math";
-        }else if(c == 'c'){
-            mode = "circle";
+        while (true) {
+            System.out.println("산수: m | 원넓이: c 를 입력해주세요: ");
+            String input = sc.next();
+            if (input != null && !input.isEmpty()) {
+                char c = input.charAt(0);
+                if (c == 'm') {
+                    mode = "math";
+                    break; // 올바른 입력이 들어온 경우 반복문 종료
+                } else if (c == 'c') {
+                    mode = "circle";
+                    break; // 올바른 입력이 들어온 경우 반복문 종료
+                } else {
+                    System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+                }
+            } else {
+                System.out.println("입력이 비어있습니다. 다시 입력해주세요.");
+            }
         }
     }
 
